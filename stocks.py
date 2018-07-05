38c3912ea097d248b6c82e406c9993816b32d477
pzc <paul.zimmerman.clayton@gmail.com>
2018-07-05 14:55:52 -0500

    stock tables done

---
+++ stocks.py
@@ -0,0 +1,12 @@
+stock_dict = {
+  'GM': 'General Motors',
+  'AAPL': 'Apple',
+  'WU': "Western Union"
+}
+
+purchases = [
+  ('GM', 800, '10-sep-2003', 50),
+  ('AAPL', 190, '15-jun-2014', 21),
+  ('WU', 20, '15-jul-2010', 49),
+  ()
+]
