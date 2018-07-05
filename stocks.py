08889c780ca406466eba9a9d9192a7559a44910e
pzc <paul.zimmerman.clayton@gmail.com>
2018-07-05 15:06:45 -0500

    part 1 done

--- stocks.py
+++ stocks.py
@@ -7,6 +7,8 @@ stock_dict = {
 purchases = [
   ('GM', 800, '10-sep-2003', 50),
   ('AAPL', 190, '15-jun-2014', 21),
-  ('WU', 20, '15-jul-2010', 49),
-  ()
+  ('WU', 20, '15-jul-2010', 49)
 ]
+
+for purchase in purchases:
+    print(f'{stock_dict[purchase[0]]}, total value: {purchase[1] * purchase[3]}')
