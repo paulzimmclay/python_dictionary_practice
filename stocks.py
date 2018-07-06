fc0f05ed6eb749846cb1b174851081e5490dba04
pzc <paul.zimmerman.clayton@gmail.com>
2018-07-06 09:07:55 -0500

    need to do the last part

--- stocks.py
+++ stocks.py
@@ -7,8 +7,16 @@ stock_dict = {
 purchases = [
   ('GM', 800, '10-sep-2003', 50),
   ('AAPL', 190, '15-jun-2014', 21),
+  ('AAPL', 190, '15-jun-2014', 4),
+  ('AAPL', 190, '15-jun-2014', 8),
   ('WU', 20, '15-jul-2010', 49)
 ]
 
 for purchase in purchases:
     print(f'{stock_dict[purchase[0]]}, total value: {purchase[1] * purchase[3]}')
+
+for purchase in purchases:
+    cumulative_purchases = {}
+    
+    cumulative_purchases.update({purchase[0]:"test"})
+    print(cumulative_purchases)
